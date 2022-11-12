05f29e3b49d5779a45df302565130da8ea088638
BuriyAnna <anyuta_r@mail.ru>
2022-11-12 20:23:13 +0300

    timer

---
+++ Timer1/TableCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableCell.swift
+//  Timer1
+//
+//  Created by user on 12.11.2022.
+//
+
+import UIKit
+
+class TableCell: UITableViewCell {
+    
+    @IBOutlet weak var tableLabel: UILabel!
+    
+}
